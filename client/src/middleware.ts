0bd98b741e7cd566d61d35ef8d8ca650b2cbb0d4
Aminur Rahman <dev.aminur@gmail.com>
2025-01-30 14:36:26 +0600

    try to implement redirect logic

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,5 +1,5 @@
 // middleware.ts
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
@@ -10,11 +10,11 @@ export function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value || "";
 
   if (isPublicPath && token) {
-    // return NextResponse.redirect(new URL("/dashboard", request.url));
+    return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
   if (!isPublicPath && !token) {
-    // return NextResponse.redirect(new URL("/login", request.url));
+    return NextResponse.redirect(new URL("/login", request.url));
   }
 }
 
